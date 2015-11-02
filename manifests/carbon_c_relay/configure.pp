aa4edd4eedadbbc5799abd0f40b860772d78efa8
Aaron Bandt <abandt@rubiconproject.com>
2015-11-02 12:43:29 -0800

    adding vars to support configurable ports. Also adding notify to the service if the conf changes

--- manifests/carbon_c_relay/configure.pp
+++ manifests/carbon_c_relay/configure.pp
@@ -5,7 +5,9 @@
 #
 class int_graphite::carbon_c_relay::configure (
   $local_cache    = $::int_graphite::params::local_cache,
-  $remote_cache   = $::int_graphite::params::remote_cache
+  $remote_cache   = $::int_graphite::params::remote_cache,
+  $port_start     = $::int_graphite::params::port_start,
+  $port_end       = $::int_graphite::params::port_end
 ) inherits int_graphite::params {
 
   file {
@@ -13,12 +15,18 @@ class int_graphite::carbon_c_relay::configure (
       owner  => 'root',
       group  => 'root',
       mode   => '0644',
-      source => 'puppet:///modules/int_graphite/carbon_c_relay/carbon-c-relay',;
+      source => 'puppet:///modules/int_graphite/carbon_c_relay/carbon-c-relay',
+      notify => Service['carbon-c-relay'],;
     '/etc/carbon-c-relay.conf':
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
-      content => template('int_graphite/carbon_c_relay/carbon-c-relay.conf.erb'),;
+      content => template('int_graphite/carbon_c_relay/carbon-c-relay.conf.erb'),
+      notify => Service['carbon-c-relay'],;
   }
 
+  service { carbon-c-relay:
+    hasrestart => false,
+    hasstatus => false,
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,5 +2,7 @@ class int_graphite::params {
 
   $local_cache = '10.31.20.31'
   $remote_cache = '10.83.16.195'
+  $port_start = 20
+  $port_end = 35
 
 }
