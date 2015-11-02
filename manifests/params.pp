d527ec8380cbeea449a50cfae0ea6d6b567a85be
Aaron Bandt <abandt@rubiconproject.com>
2015-11-02 14:59:51 -0800

    missing trailing parens

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,8 @@
 class int_graphite::params {
 
-  $local_cache    = hiera("int_graphite::params::local_cache"
-  $remote_cache   = hiera("int_graphite::params::remote_cache"
-  $port_start     = hiera("int_graphite::params::port_start"
-  $port_end       = hiera("int_graphite::params::port_end"
+  $local_cache    = hiera("int_graphite::params::local_cache")
+  $remote_cache   = hiera("int_graphite::params::remote_cache")
+  $port_start     = hiera("int_graphite::params::port_start")
+  $port_end       = hiera("int_graphite::params::port_end")
 
 }
