316bf9165690d2baa6be98a071270ea6eccf5bb1
Sean Byron <sbyron@rubiconproject.com>
2016-01-08 15:18:33 -0800

    INFRA-1551 - Starting work on replacing graphite_storage with int_graphite::carbon_cache

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,4 +12,9 @@ class int_graphite::params {
   $carbon_c_relay_state  = hiera('int_graphite::params::carbon_c_relay_state')
   $carbon_c_relay_enable = hiera('int_graphite::params::carbon_c_relay_enable')
 
+  # OS-based variables (see hieradata/operatingsystem)
+  $carbon_cache_version = hiera('int_graphite::params::carbon_cache_version')
+  $carbon_cache_package_requires = hiera('int_graphite::params::carbon_cache_package_requires')
+  $carbon_cache_packages = hiera('int_graphite::params::carbon_cache_packages')
+
 }
