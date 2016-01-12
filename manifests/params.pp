ea3bcb8f5a727e3802338f3b776d0ae110d79330
Sean Byron <sbyron@rubiconproject.com>
2016-01-12 13:31:16 -0800

    INFRA-1551 - Starting work on replacing graphite_storage with int_graphite::carbon_cache

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,7 +17,6 @@ class int_graphite::params {
   $carbon_cache_package_requires = hiera('int_graphite::params::carbon_cache_package_requires')
   $carbon_cache_packages = hiera('int_graphite::params::carbon_cache_packages')
 
-
   # Resource arrays
   $carbon_cache_daemon_subscribe = ['/opt/graphite/conf/storage-aggregation.conf', '/opt/graphite/conf/storage-schemas.conf', '/etc/security/limits.d/graphite_storage.conf', '/etc/init.d/carbon-cache', '/opt/graphite/conf/carbon.conf', ],
   $carbon_cache_httpd_subscribe = ['/etc/sysconfig/httpd', '/etc/httpd/conf/httpd.conf', '/etc/httpd/conf.d/auth_kerb.conf', '/etc/httpd/conf.d/graphite.conf', '/etc/httpd/conf.d/ssl.conf', '/etc/pki/tls/private/localhost.key', '/etc/pki/tls/certs/localhost.crt', '/opt/graphite/webapp/graphite/local_settings.py', ],
