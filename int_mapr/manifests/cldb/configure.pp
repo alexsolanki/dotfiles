df41a4fd638dedeb9a3078e231432bddfb6f039c
Jesse Escobedo <jescobedo@rubiconproject.com>
2016-02-09 14:30:33 -0800

    DP-1439: and that's why we don't call classes inside of core classes

--- int_mapr/manifests/cldb/configure.pp
+++ int_mapr/manifests/cldb/configure.pp
@@ -4,21 +4,8 @@
 #   Class for MapR CLDB configuration
 #
 class int_mapr::cldb::configure (
-  $cluster_name = hiera('int_mapr::cluster_name'),
-  $cldb_hosts = hiera('int_mapr::cldb_hosts'),
-  $zookeeper_hosts = hiera('int_mapr::zookeeper_hosts'),
   $mapr_ensure_service = false,
 ) {
-
-  # Configure a cldb node
-  class { 'int_mapr::buildconf':
-    cluster_name    => $cluster_name,
-    cldb_hosts      => $cldb_hosts,
-    zookeeper_hosts => $zookeeper_hosts,
-  }
-
-  # Validate a cldb node
-  class { 'int_mapr::validate': }
 
   if $mapr_ensure_service == true {
     service {
