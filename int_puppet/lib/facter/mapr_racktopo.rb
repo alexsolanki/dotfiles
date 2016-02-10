7477f9dc31134835a05ce7e79bd8db5d45ed654f
Jesse Escobedo <jescobedo@rubiconproject.com>
2016-02-10 14:37:12 -0800

    DP-1439: attempt to make things work with proper end tags

--- int_puppet/lib/facter/mapr_racktopo.rb
+++ int_puppet/lib/facter/mapr_racktopo.rb
@@ -5,114 +5,118 @@
 # http://wiki.rubiconproject.com/display/techops/MapR+DataNode+Verification+Process
 
 Facter.add("mapr_racktopo") do
-    confine :fanrole => 'cld'
-    setcode do
-      retval = "/cldb"
-    end
-    confine :fanrole => 'jtr'
-    setcode do
-      retval = ""
-      case Facter.value(:fanhostgroupalphaonly)
+  confine :fanrole => 'cld'
+  setcode do
+    retval = "/cldb"
+    # Return retval
+    retval
+  end
+  confine :fanrole => 'jtr'
+  setcode do
+    retval = ""
+    case Facter.value(:fanhostgroupalphaonly)
       when "fmap-jtr"
-          retval = "/jobtracker"
+        retval = "/jobtracker"
       else
-          retval = "/resourcemanager"
-      end
+        retval = "/resourcemanager"
     end
-    confine :fanrole => 'com'
-    setcode do
-      retval = ""
-      case Facter.value(:fandatacenter)
+    # Return retval
+    retval
+  end
+  confine :fanrole => 'com'
+  setcode do
+    retval = ""
+    case Facter.value(:fandatacenter)
       when "las2"
-          case Facter.value(:fanhostgroupalphaonly)
+        case Facter.value(:fanhostgroupalphaonly)
           when "fmap-com"
-              case Facter.value(:fanhostsequence).to_i
+            case Facter.value(:fanhostsequence).to_i
               when 1000..1029
-                  rack_number = 5607
+                rack_number = 5607
               when 1030..1059
-                  rack_number = 5606
+                rack_number = 5606
               when 1060..1089
-                  rack_number = 5605
+                rack_number = 5605
               when 1090..1119
-                  rack_number = 5604
+                rack_number = 5604
               when 1120..1149
-                  rack_number = 5615
+                rack_number = 5615
               when 1150..1179
-                  rack_number = 5616
+                rack_number = 5616
               when 1180..1209
-                  rack_number = 5617
+                rack_number = 5617
               when 1210..1239
-                  rack_number = 5618
-              end
+                rack_number = 5618
+            end
 
-              if rack_number.to_s.length > 0
-                # We know what rack if it matched a case and was assigned
-                retval = "/data/rack" + rack_number.to_s
-              else
-                # When the fanhostsequence does not match a range
-                retval = "undefined"
-              end
+            if rack_number.to_s.length > 0
+              # We know what rack if it matched a case and was assigned
+              retval = "/data/rack" + rack_number.to_s
+            else
+              # When the fanhostsequence does not match a range
+              retval = "undefined"
+            end
           when "fmas-com"
-              case Facter.value(:fanhostsequence).to_i + 20000
+            case Facter.value(:fanhostsequence).to_i + 20000
               when 20000..20026
-                  rack_number = 5622
+                rack_number = 5622
               when 20027..20053
-                  rack_number = 5621
+                rack_number = 5621
               when 20054..20080
-                  rack_number = 5620
-              end
+                rack_number = 5620
+            end
 
-              if rack_number.to_s.length > 0
-                # We know what rack if it matched a case and was assigned
-                retval = "/data/rack" + rack_number.to_s
-              else
-                # When the fanhostsequence does not match a range
-                retval = "undefined"
-              end
+            if rack_number.to_s.length > 0
+              # We know what rack if it matched a case and was assigned
+              retval = "/data/rack" + rack_number.to_s
+            else
+              # When the fanhostsequence does not match a range
+              retval = "undefined"
+            end
           when "fbmq-com"
-              case Facter.value(:fanhostsequence).to_i + 20000
+            case Facter.value(:fanhostsequence).to_i + 20000
               when 20000..20006
-                  rack_number = 1201
+                rack_number = 1201
               when 20007..20013
-                  rack_number = 1202
+                rack_number = 1202
               when 20014..20019
-                  rack_number = 1203
-              end
+                rack_number = 1203
+            end
 
-              if rack_number.to_s.length > 0
-                # We know what rack if it matched a case and was assigned
-                retval = "/data/rack" + rack_number.to_s
-              else
-                # When the fanhostsequence does not match a range
-                retval = "undefined"
-              end
+            if rack_number.to_s.length > 0
+              # We know what rack if it matched a case and was assigned
+              retval = "/data/rack" + rack_number.to_s
+            else
+              # When the fanhostsequence does not match a range
+              retval = "undefined"
+            end
           else
             # When the fanhostgroupalpha doesn't match a case
             retval = "undefined"
           end
       when "lab1"
-          case Facter.value(:fanhostgroupalphaonly)
+        case Facter.value(:fanhostgroupalphaonly)
           when "fmad-com"
-              case Facter.value(:fanhostsequence).to_i
+            case Facter.value(:fanhostsequence).to_i
               when 9000..9050
-                  rack_number = 9000
-              end
-              if rack_number.to_s.length > 0
-                # We know what rack if it matched a case and was assigned
-                retval = "/data/rack" + rack_number.to_s
-              else
-                # When the fanhostsequence does not match a range
-                retval = "undefined"
-              end
+                rack_number = 9000
+            end
+            if rack_number.to_s.length > 0
+              # We know what rack if it matched a case and was assigned
+              retval = "/data/rack" + rack_number.to_s
+            else
+              # When the fanhostsequence does not match a range
+              retval = "undefined"
+            end
           else
             # When the fanhostgroupalpha doesn't match a case
             retval = "undefined"
-          end
+        end
       else
         # When the datacenter doesn't match a case
         retval = "undefined"
-      end
-      # Return retval
-      retval
     end
+    # Return retval
+    retval
+  end
 end
