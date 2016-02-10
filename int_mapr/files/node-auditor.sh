a4f179dc1211ff0fda72f0eeff06602790a7c677
Jesse Escobedo <jescobedo@rubiconproject.com>
2016-02-10 13:20:46 -0800

    DP-1439: adding fix for iozone logs

--- int_mapr/files/node-auditor.sh
+++ int_mapr/files/node-auditor.sh
@@ -84,7 +84,8 @@ memory_test() {
 disk_test() {
   echo -e "INFO: Running disk test"
   echo -e "INFO: Begin disk performance validation." >> $BASEPATH/logs/disk-test.log
-
+  #FIXME: Need to update RPM to safe logs in logs dir
+  cd $BASEPATH/logs
   for i in {1..$DISKTESTRUNS}; do
     $BASEPATH/pre-install/disk-test.sh > $BASEPATH/logs/disk-test.out.$i 2>&1
     /bin/grep -F '   1024' sd*-iozone.log | awk '{ print $1, $4, $5 }' > $BASEPATH/logs/results.log.$i 2>&1
